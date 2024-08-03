b3b2f5f9630f6f659577791fb826db405d5cfbcc
Caden Miller <cwmiller4030@proton.me>
2024-08-03 11:18:58 -0500

    small change for UciCommands

--- NuvoChessSharp/UniversalChessInterface/UciCommands.cs
+++ NuvoChessSharp/UniversalChessInterface/UciCommands.cs
@@ -2,7 +2,7 @@ namespace NuvoChessSharp.UniversalChessInterface;
 
 public static class UciCommands
 {
-    public static readonly string Quit = "quit";
+    public const string Quit = "quit";
     public const string Position = "position";
     public const string StartPosition = "startpos";
     public const string Print = "print";
